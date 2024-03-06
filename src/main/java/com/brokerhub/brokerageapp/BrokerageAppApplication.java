69abb6b5ce40c9327cf2f6d2fdf5dad311b1074a
TARUN BATCHU <tarunbatchu2000@gmail.com>
2024-03-06 17:20:56 +0530

    Initial commit

---
+++ src/main/java/com/brokerhub/brokerageapp/BrokerageAppApplication.java
@@ -0,0 +1,13 @@
+package com.brokerhub.brokerageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brokerhub/brokerageapp/BrokerageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokerhub.brokerageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
