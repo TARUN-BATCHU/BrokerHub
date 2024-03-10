236c800239d426b9166f1f868c028545bb3309bf
TARUN BATCHU <tarunbatchu2000@gmail.com>
2024-03-10 20:39:34 +0530

    created entities

---
+++ src/main/java/com/brokerhub/brokerageapp/entity/Address.java
@@ -0,0 +1,60 @@
+package com.brokerhub.brokerageapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    private String city;
+
+    private String area;
+
+    private String shopNumber;
+
+    private String pincode;
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getShopNumber() {
+        return shopNumber;
+    }
+
+    public void setShopNumber(String shopNumber) {
+        this.shopNumber = shopNumber;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+}
---
+++ src/main/java/com/brokerhub/brokerageapp/entity/User.java
@@ -0,0 +1,145 @@
+package com.brokerhub.brokerageapp.entity;
+
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "user_type", insertable = false, updatable = false)
+    private String userType;
+
+    private String gstNumber;
+
+    private String firmName;
+
+    private String ownerName;
+
+    private Address address;
+
+    private BankDetails bankDetails;
+
+    private List<String> phoneNumbers;
+
+    private int brokerageRate;
+
+    private Long totalBagsSold;
+
+    private Long totalBagsBought;
+
+    private Long payableAmount;
+
+    private Long receivableAmount;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getGstNumber() {
+        return gstNumber;
+    }
+
+    public void setGstNumber(String gstNumber) {
+        this.gstNumber = gstNumber;
+    }
+
+    public String getFirmName() {
+        return firmName;
+    }
+
+    public void setFirmName(String firmName) {
+        this.firmName = firmName;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public BankDetails getBankDetails() {
+        return bankDetails;
+    }
+
+    public void setBankDetails(BankDetails bankDetails) {
+        this.bankDetails = bankDetails;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public int getBrokerageRate() {
+        return brokerageRate;
+    }
+
+    public void setBrokerageRate(int brokerageRate) {
+        this.brokerageRate = brokerageRate;
+    }
+
+    public Long getTotalBagsSold() {
+        return totalBagsSold;
+    }
+
+    public void setTotalBagsSold(Long totalBagsSold) {
+        this.totalBagsSold = totalBagsSold;
+    }
+
+    public Long getTotalBagsBought() {
+        return totalBagsBought;
+    }
+
+    public void setTotalBagsBought(Long totalBagsBought) {
+        this.totalBagsBought = totalBagsBought;
+    }
+
+    public Long getPayableAmount() {
+        return payableAmount;
+    }
+
+    public void setPayableAmount(Long payableAmount) {
+        this.payableAmount = payableAmount;
+    }
+
+    public Long getReceivableAmount() {
+        return receivableAmount;
+    }
+
+    public void setReceivableAmount(Long receivableAmount) {
+        this.receivableAmount = receivableAmount;
+    }
+}
