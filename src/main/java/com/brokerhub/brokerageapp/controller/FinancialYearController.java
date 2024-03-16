daa883edaa98afb207cb5e71345bc949b731d574
TARUN BATCHU <tarunbatchu2000@gmail.com>
2024-03-16 14:35:45 +0530

    initial setup of controllers

--- src/main/java/com/brokerhub/brokerageapp/controller/FinancialYearController.java
+++ src/main/java/com/brokerhub/brokerageapp/controller/FinancialYearController.java
@@ -1,4 +1,9 @@
 package com.brokerhub.brokerageapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/BrokerHub/FinancialYear")
 public class FinancialYearController {
 }
--- src/main/java/com/brokerhub/brokerageapp/controller/UserController.java
+++ src/main/java/com/brokerhub/brokerageapp/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.brokerhub.brokerageapp.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/BrokerHub/User")
 public class UserController {
+
 }
