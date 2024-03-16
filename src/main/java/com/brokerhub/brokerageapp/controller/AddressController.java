8b15d8f282af4a36ea8d7308168a1cf69f941a18
TARUN BATCHU <tarunbatchu2000@gmail.com>
2024-03-16 15:32:25 +0530

    autowired service

--- src/main/java/com/brokerhub/brokerageapp/controller/AddressController.java
+++ src/main/java/com/brokerhub/brokerageapp/controller/AddressController.java
@@ -1,9 +1,16 @@
 package com.brokerhub.brokerageapp.controller;
 
+import com.brokerhub.brokerageapp.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/BrokerHub/Address")
 public class AddressController {
+
+    @Autowired
+    AddressService addressService;
+
+
 }
