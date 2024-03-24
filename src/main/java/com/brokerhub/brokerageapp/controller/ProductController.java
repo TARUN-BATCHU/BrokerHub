12b578cd4da66a8b6da7609cf5415ca597b45d63
TARUN BATCHU <tarunbatchu2000@gmail.com>
2024-03-24 16:44:20 +0530

    create product, update product, delete product, get all products, get all products by Name

--- src/main/java/com/brokerhub/brokerageapp/controller/ProductController.java
+++ src/main/java/com/brokerhub/brokerageapp/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.brokerhub.brokerageapp.controller;
 
+import com.brokerhub.brokerageapp.entity.Product;
 import com.brokerhub.brokerageapp.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/BrokerHub/Product")
@@ -11,4 +17,29 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+
+    @PostMapping("/createProduct")
+    public ResponseEntity createProduct(@Valid @RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/updateProduct")
+    public Object updateProduct(@Valid @RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/deleteProduct")
+    public ResponseEntity deleteProduct(@RequestParam Long productId){
+        return productService.deleteProduct(productId);
+    }
+
+    @GetMapping("/allProducts")
+    public List<Product> getAllProducts(Pageable pageable){
+        return productService.getAllProducts(pageable);
+    }
+
+    @GetMapping("/allProducts/")
+    public List<Product> getAllProductsByName(@RequestParam String productName){
+        return productService.getAllProductsByName(productName);
+    }
 }
