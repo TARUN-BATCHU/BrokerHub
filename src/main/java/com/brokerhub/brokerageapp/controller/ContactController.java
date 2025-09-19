f0df175e9fe6c8968d050be926b7a3eb9daaacf3
TARUN BATCHU <tarunbatchu2000@gmail.com>
2025-09-19 18:20:06 +0530

    Contacts API's

--- src/main/java/com/brokerhub/brokerageapp/controller/ContactController.java
+++ src/main/java/com/brokerhub/brokerageapp/controller/ContactController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/contacts")
+@RequestMapping("/BrokerHub/api/contacts")
 @RequiredArgsConstructor
 @Slf4j
 public class ContactController {
