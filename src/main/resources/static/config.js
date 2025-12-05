bf1cf32d31750e868316e28973bc87f69486942d
TARUN BATCHU <tarunbatchu2000@gmail.com>
2025-12-05 14:01:06 +0530

    multi device use fixes

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -1,3 +1,4 @@
 window.APP_CONFIG = {
-  API_URL: 'http://192.168.0.107:8080/BrokerHub'
+  API_URL: `http://${window.location.hostname}:8080/BrokerHub`
 };
+console.log('BrokerHub API URL:', window.APP_CONFIG.API_URL);
